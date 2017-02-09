7a6cc5decef7fb497ec287233cf4a3d7fae11c35
JoshAGordon <joshuagordon8@hotmail.com>
2017-02-09 10:38:40 +0000

    Push JUnit Test

---
+++ src/test/java/com/qa/bookstore/student5/BookServiceTest.java
@@ -0,0 +1,17 @@
+package com.qa.bookstore.student5;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BookServiceTest {
+
+	@Test
+	public void testHowManyBooksStephenKingHasInStore() {
+		BookService service = new BookService();
+		Book IT = new Book("Stephen King", "IT", "Horror", "1988");
+		service.addBook(IT);
+		long count = service.howManyBooksByAuthor("Stephen King");
+		Assert.assertEquals(count, 1);
+	}
+
+}
